10c4ea147e813587d00ee1236a799f3821474cf7
Jason Zhou <jason.zhou.design@gmail.com>
2025-06-23 01:34:55 +1000

    Move clear chat button to subtle top-right position
    - Move clear button from input actions to top-right corner of input area
    - Make button small, subtle with no background (16x16px)
    - Change icon to refresh/restart symbol for clarity
    - Add right padding to message input to avoid overlap
    - Button only visible on hover with smooth animations

--- src/webview/components/Chat/ChatInterface.tsx
+++ src/webview/components/Chat/ChatInterface.tsx
@@ -779,6 +779,17 @@ const ChatInterface: React.FC<ChatInterfaceProps> = ({ layout, vscode }) => {
                                 className="message-input"
                                 rows={1}
                             />
+                            <button 
+                                className="clear-chat-btn"
+                                onClick={handleNewConversation}
+                                disabled={isLoading}
+                                title="Clear chat and start fresh"
+                            >
+                                <svg width="12" height="12" viewBox="0 0 16 16" fill="currentColor">
+                                    <path d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41zm-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9z"/>
+                                    <path fillRule="evenodd" d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5.002 5.002 0 0 0 8 3zM3.1 9a5.002 5.002 0 0 0 4.9 4c1.552 0 2.94-.707 3.857-1.818a.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9H3.1z"/>
+                                </svg>
+                            </button>
                         </div>
 
                         {/* Agent and Model Selectors with Actions */}
@@ -817,17 +828,6 @@ const ChatInterface: React.FC<ChatInterfaceProps> = ({ layout, vscode }) => {
                             </div>
                             
                             <div className="input-actions">
-                                <button 
-                                    className="clear-chat-btn"
-                                    onClick={handleNewConversation}
-                                    disabled={isLoading}
-                                    title="Clear chat and start fresh"
-                                >
-                                    <svg width="16" height="16" viewBox="0 0 16 16" fill="currentColor">
-                                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-                                        <path fillRule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-                                    </svg>
-                                </button>
                                 <button 
                                     className="attach-btn"
                                     disabled={isLoading}
